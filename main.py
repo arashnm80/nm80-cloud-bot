5f3481876670ae0bd5653ebec734ca78a1ec559a
Arash Nemat Zadeh <arashnm80@gmail.com>
2023-01-26 11:15:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,38 @@ def get_list():
     i = 0
     with open("list") as file:
         for line in file:
+            line = line.rstrip('\n')
             i += 1
-            list.append(line.rstrip('\n'))
-    return list 
+            new_id = line.split()[0]
+            if new_id == line:
+                new_summary = "empty"
+            else:
+                new_summary = line.split(' ', 1)[1]
+            list.append([new_id, new_summary])
+    return list
 
 @bot.message_handler(commands = ['start'])
 def start_message(message):
     bot.send_message(message.chat.id, "welcome to nm80 cloud bot")
+    print(get_list())
 
 @bot.message_handler(commands= ['add'])
 def add(message):
     if message.reply_to_message != None:
-        bot.reply_to(message, "message " + str(message.reply_to_message.message_id) + " added to cloud.")
-        file = open("list","a+")
-        file.write(str(message.reply_to_message.message_id))
-        file.write("\n")
-        file.close()
+        replied_message_id = message.reply_to_message.message_id
+        exists = False
+        for item in get_list():
+            if str(replied_message_id) == item[0]:
+                exists = True
+                break
+        if not exists:
+            bot.reply_to(message, "message " + str(replied_message_id) + " added to cloud.")
+            file = open("list","a+")
+            file.write(str(message.reply_to_message.message_id))
+            file.write("\n")
+            file.close()
+        else:
+            bot.send_message(message.chat.id, "message " + str(replied_message_id) + " already exists in cloud.")
     else:
         bot.reply_to(message, "no reference for /add command")
 
@@ -38,9 +54,10 @@ def add(message):
 @bot.message_handler(regexp="^/\d+$")
 def get_by_index(message):
     index = int(message.text[1:])
-    id = get_list()[index - 1]
+    id = get_list()[index - 1][0]
     bot.send_message(message.chat.id, "message with  \"index /" + str(index) + "\"  &  \"id " + str(id) + "\"  :")
-    bot.forward_message(chat_id = message.chat.id, from_chat_id = message.chat.id, message_id = id)
+#    bot.forward_message(chat_id = message.chat.id, from_chat_id = message.chat.id, message_id = id)
+    bot.copy_message(chat_id = message.chat.id, from_chat_id = message.chat.id, message_id = id)
 
 @bot.message_handler(commands = ['list'])
 def list_command(message):
@@ -49,7 +66,7 @@ def list_command(message):
     i = 0
     for item in list:
         i += 1
-        contents_message += "/" + str(i) + "  " + item + "\n"
+        contents_message += "/" + str(i) + "  " + item[0] + "\n"
     bot.send_message(message.chat.id, contents_message)
 
 bot.infinity_polling()
